7983741e23823bfb0090a99d5421d783210b90c6
Albert Wang <albertwang@gmail.com>
2022-02-07 20:45:56 -0500

    add total_confirmed

--- airbyte_dbt_test/models/covid_test.sql
+++ airbyte_dbt_test/models/covid_test.sql
@@ -7,6 +7,7 @@ with covid_epidemiology_ab1 as (
     json_extract(_airbyte_data, '$.new_tested')  as new_tested,
     json_extract(_airbyte_data, '$.new_deceased')  as new_deceased,
     json_extract(_airbyte_data, '$.total_tested')  as total_tested,
+    json_extract(_airbyte_data, '$.total_confirmed')  as total_confirmed,
     json_extract(_airbyte_data, '$.new_confirmed')  as new_confirmed
 
 from `albert_1mo_net`.`_airbyte_raw_covid_covid_epidemiology`
